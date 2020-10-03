8d3bcda8ccce6c8542e5e478db720c02a6a782b5
Connor Peavey <cspgit@twinleaf.xyz>
2020-10-02 22:48:12 -0400

    First real commit
    Basic level project

---
+++ Assets/Scripts/JumpCorrection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class JumpCorrection : MonoBehaviour
+{
+    [SerializeField] float fallMultiplier = 2.5f;
+    [SerializeField] float lowJumpMultiplier = 2f;
+
+    public Rigidbody2D myRigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        adjustFallTime();
+        adjustLowJump();
+    }
+
+    void adjustFallTime()
+    {
+        if (myRigidBody.velocity.y < 0)
+        {
+            myRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+    void adjustLowJump()
+    {
+        GameObject player = GameObject.Find("Player");
+        Player playerScript = player.GetComponent<Player>();
+
+
+        if (myRigidBody.velocity.y > 0 && !CrossPlatformInputManager.GetButton("Jump"))
+
+        {
+            myRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    //stat values
+    [SerializeField] float maxRunSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float maxFallSpeed = -20f;
+    float runSpeed = 0f;
+
+    //common bools
+    public bool isGrounded;
+
+    //set up components
+    public Rigidbody2D myRigidBody;
+    public CapsuleCollider2D myBodyCollider;
+    public BoxCollider2D myFeet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckSurroundings();
+        Run();
+        FlipSprite();
+        Jump();
+    }
+
+    private void CheckSurroundings()
+    {
+        if (myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        if (myRigidBody.velocity.y <= maxFallSpeed)
+        {
+            float currentFallSpeed = myRigidBody.velocity.y;
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, currentFallSpeed);
+        }
+    }
+
+    private void Run()
+    {
+
+        float controlThrow = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+
+        if (Mathf.Abs(controlThrow) > 0)
+        {
+            runSpeed += 0.6f;
+            if (runSpeed >= maxRunSpeed)
+            {
+                runSpeed = maxRunSpeed;
+            }
+            myRigidBody.velocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        }
+        else
+        {
+            if (transform.localScale.x > 0)
+            {
+                myRigidBody.velocity = new Vector2(runSpeed, myRigidBody.velocity.y);
+            }
+            else if (transform.localScale.x < 0)
+            {
+                myRigidBody.velocity = new Vector2(runSpeed * -1, myRigidBody.velocity.y);
+            }
+            if (runSpeed > 0)
+                runSpeed -= 1f;
+            if (runSpeed <= 0)
+            {
+                runSpeed = 0;
+            }
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpSpeed);
+                if (myRigidBody.velocity.x > 0)
+                {
+                    transform.localScale = new Vector2(1, 1);
+                }
+                else if (myRigidBody.velocity.x < 0)
+                {
+                    transform.localScale = new Vector2(-1, 1);
+                }
+            }
+        }
+    }
+
+    private void FlipSprite()
+    {
+        if (myRigidBody.velocity.x > 0 && (isGrounded || myRigidBody.velocity.y > 0))
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        if (myRigidBody.velocity.x < 0 && (isGrounded || myRigidBody.velocity.y > 0))
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+    }
+
+   
+}
+
+
